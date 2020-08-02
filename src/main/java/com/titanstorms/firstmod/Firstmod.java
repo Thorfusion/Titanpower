3f705bf2f4b027fc67e81d570f33941a60403f30
maggi373 <40539743+maggi373@users.noreply.github.com>
2020-08-02 17:27:54 +0200

    Update Firstmod.java

--- src/main/java/com/titanstorms/firstmod/Firstmod.java
+++ src/main/java/com/titanstorms/firstmod/Firstmod.java
@@ -82,7 +82,7 @@ public class Firstmod
       {
 			//proxy, tilentity
 			if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-				DevCapes.getInstance().registerConfig("https://dl.dropboxusercontent.com/u/22865035/ModHosting/capes/capes.json");
+				DevCapes.getInstance().registerConfig("https://technic-solder.eu-central-1.linodeobjects.com/cape.json");
 			}
       }
       @EventHandler
