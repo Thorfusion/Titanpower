f47fa42e835b936124730d161954365b46e5fa87
Magnar <40539743+maggi373@users.noreply.github.com>
2020-08-02 16:29:33 +0200

    Create TickHandler.java

---
+++ src/main/java/com/titanstorms/firstmod/TickHandler.java
@@ -0,0 +1,13 @@
+import mekanism.common.util.MekanismUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.entity.player.EntityPlayer;
+
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+
+import cpw.mods.fml.client.FMLClientHandler;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.ClientTickEvent;
+import cpw.mods.fml.common.gameevent.TickEvent.Phase;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
