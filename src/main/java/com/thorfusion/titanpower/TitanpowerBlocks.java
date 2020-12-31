b76d64ce1c83b148890127c7cbc0b005616b9c1c
maggi373 <40539743+maggi373@users.noreply.github.com>
2020-12-31 01:22:26 +0100

    woops

--- src/main/java/com/thorfusion/titanpower/TitanpowerBlocks.java
+++ src/main/java/com/thorfusion/titanpower/TitanpowerBlocks.java
@@ -11,20 +11,21 @@ public class TitanpowerBlocks {
 	public static void init() {
 		titandia = new Titandia();
 	}
-}
-public class Titandia  extends Block {
-	private static final String name = "titandia";
 
-	public Titandia() {
-		super(Material.iron);
-		GameRegistry.registerBlock(this, name);
-		setBlockTextureName(Titanpower.MODID + ":" + name);
-		setCreativeTab(Titanpower.tabTitanpower);
-		setBlockName(name);
-		setLightLevel(1F);
-		setStepSound(soundTypeSand);
-		setResistance(4000F);
-		setHardness(0.1F);
-		setHarvestLevel("shovel", 3);
+	public class Titandia extends Block {
+		private static final String name = "titandia";
+
+		public Titandia() {
+			super(Material.iron);
+			GameRegistry.registerBlock(this, name);
+			setBlockTextureName(Titanpower.MODID + ":" + name);
+			setCreativeTab(Titanpower.tabTitanpower);
+			setBlockName(name);
+			setLightLevel(1F);
+			setStepSound(soundTypeSand);
+			setResistance(4000F);
+			setHardness(0.1F);
+			setHarvestLevel("shovel", 3);
+		}
 	}
 }
