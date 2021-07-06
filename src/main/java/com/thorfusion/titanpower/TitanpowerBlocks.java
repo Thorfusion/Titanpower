d77554c6070a5b22d051937494f624d0fa922fdc
maggi373 <40539743+maggi373@users.noreply.github.com>
2021-07-06 20:06:36 +0200

    add torch files

--- src/main/java/com/thorfusion/titanpower/TitanpowerBlocks.java
+++ src/main/java/com/thorfusion/titanpower/TitanpowerBlocks.java
@@ -6,8 +6,12 @@ import net.minecraft.block.Block;
 
 public class TitanpowerBlocks {
 	public static Block titandia;
+	public static Block blutorch;
 
 	public static void init() {
 		titandia = new Titandia();
+	}
+	public static void init() {
+		blutorch = new Blutorch();
 	}
 }
