1594f0943d9468371ce55cf28d6eced06a57481a
maggi373 <40539743+maggi373@users.noreply.github.com>
2022-05-22 02:59:23 +0200

    code cleanup

--- src/main/java/com/thorfusion/titanpower/Titanpower.java
+++ src/main/java/com/thorfusion/titanpower/Titanpower.java
@@ -1,23 +1,23 @@
 package com.thorfusion.titanpower;
 
 
+import com.jadarstudios.developercapes.DevCapes;
+import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import cpw.mods.fml.common.FMLCommonHandler;
-import cpw.mods.fml.relauncher.Side;
-import com.jadarstudios.developercapes.DevCapes;
 
 
 @Mod(modid = Titanpower.MODID, name = Titanpower.NAME, version = Titanpower.VERSION)
 
-public class Titanpower{
+public class Titanpower {
     public static final String NAME = "Titanpower";
     public static final String MODID = "thorfusion";
     public static final String VERSION = "GRADLE_MODVERSION";
@@ -48,34 +48,31 @@ public class Titanpower{
         TitanpowerRecipes.init();
         TitanpowerArmour.init();
         TitanpowerArmour.register();
-        if(!isMekanismLoaded) {
+        if (!isMekanismLoaded) {
             TitanpowerVanillaRecipes.init();
         }
-        if(isMekanismLoaded) {
+        if (isMekanismLoaded) {
             TitanpowerMekanismRecipes.init();
             System.out.println("Lets bring this together mekanism");
         }
-        if(TitanpowerConfig.Titanpoweroregeneration) {
+        if (TitanpowerConfig.Titanpoweroregeneration) {
             try {
                 GameRegistry.registerWorldGenerator(new TitanpowerOreGen(), 0);
-            }
-            catch (Exception e)
-            {
-                System.out.print("World Generation Error\n"+e);
+            } catch (Exception e) {
+                System.out.print("World Generation Error\n" + e);
             }
         }
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        if(TitanpowerConfig.EnableCapes) {
+        if (TitanpowerConfig.EnableCapes) {
             //proxy, tilentity
             if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
                 try {
                     DevCapes.getInstance().registerConfig("https://raw.githubusercontent.com/maggi373/files/main/capes/cape.json");
-                } catch (Exception e)
-                {
-                    System.out.print("Failed to load capes\n"+e);
+                } catch (Exception e) {
+                    System.out.print("Failed to load capes\n" + e);
                 }
             }
         }
@@ -83,7 +80,7 @@ public class Titanpower{
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        if(isMekanismLoaded) {
+        if (isMekanismLoaded) {
             TitanpowerMekanismRecipes.postinit();
         }
 
--- src/main/java/com/thorfusion/titanpower/TitanpowerOreBlocks.java
+++ src/main/java/com/thorfusion/titanpower/TitanpowerOreBlocks.java
@@ -2,24 +2,24 @@ package com.thorfusion.titanpower;
 
 
 import com.thorfusion.titanpower.ore.Bluradien;
-import com.thorfusion.titanpower.ore.Bluradiennether;
 import com.thorfusion.titanpower.ore.Bluradienend;
+import com.thorfusion.titanpower.ore.Bluradiennether;
 import net.minecraft.block.Block;
 
 public class TitanpowerOreBlocks {
-	public static Block bluradien;
-	public static Block bluradiennether;
-	public static Block bluradienend;
+    public static Block bluradien;
+    public static Block bluradiennether;
+    public static Block bluradienend;
 
-	public static void init() {
-		bluradien = new Bluradien();
-		bluradiennether = new Bluradiennether();
-		bluradienend = new Bluradienend();
-	}
+    public static void init() {
+        bluradien = new Bluradien();
+        bluradiennether = new Bluradiennether();
+        bluradienend = new Bluradienend();
+    }
 
-	public static void register() {
+    public static void register() {
 
-	}
+    }
 }
 
 
